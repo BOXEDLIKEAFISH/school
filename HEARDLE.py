3b50494484e35ce5cf7624600ec3ad6bc2ce2c51
BOXEDLIKEAFISH <kriskuo6688@gmail.com>
2022-11-23 17:13:34 -0500

    AMOGUS

--- HEARDLE.py
+++ HEARDLE.py
@@ -1,17 +1,35 @@
-from tkinter import *
+# Kris Kuo
+# GUI Trivia Game
+# Heardle
+# November 23, 2022
+# ICS4U1
+# Ms. Townshend
+
+from tkinter import *   # import libraries
 import tkinter.ttk as ttk
 import time
 import random
 from pygame import mixer
+from PIL import ImageTk, Image
 
-black = '#121212'
+black = '#121212'   # hex codes for colours
 grey = '#BEBDB8'
 green = '#00FF00'
 red = '#FF0000'
 
 gameovermessage = ['Unlucky!', 'Nice try!', 'Michael moment.', 'Good effort!', 'Better luck next time!', 'You can do better!', 'That\'s barely a pass.', 'Almost there!', 'Nice!', 'Almost perfect!', 'Perfect!']
 
-a = 0
+songs = [
+['bert.wav', 'bertyyyyyyyyyyyyyyy', 'charles', 'ryan', 'miggle', green, red, red, red, 7],
+['bert.wav', 'fishy', 'shesh', 'ri call ryna', 'migga', red, red, green, red, 7]
+]
+
+songorder = random.sample(range(len(songs)), len(songs))
+buttonorder = random.sample(range(1, 5), 4)
+
+
+a = songorder[0]   # variable for song number
+
 
 def movebar(song, audiolength):
     global a
@@ -27,15 +45,17 @@ def movebar(song, audiolength):
 def answer(buttonpressed):
     global a
     global points
-    if songs[a][buttonpressed + 4] == green:
+    global buttonorder
+    if songs[a][4 + buttonorder[buttonpressed - 1]] == green:
         points.set(int(points.get()) + 1)
-    if a < len(songs):
-        a += 1
-    if a < len(songs):
-        uno.config(text = songs[a][1], activebackground = songs[a][5])
-        dos.config(text = songs[a][2], activebackground = songs[a][6])
-        tres.config(text = songs[a][3], activebackground = songs[a][7])
-        quatro.config(text = songs[a][4], activebackground = songs[a][8])
+    if len(songorder) > 1:
+        del(songorder[0])
+        a = songorder[0]
+        buttonorder = random.sample(range(1, 5), 4)
+        uno.config(text = songs[a][buttonorder[0]], activebackground = songs[a][buttonorder[0] + 4])
+        dos.config(text = songs[a][buttonorder[1]], activebackground = songs[a][buttonorder[1] + 4])
+        tres.config(text = songs[a][buttonorder[2]], activebackground = songs[a][buttonorder[2] + 4])
+        quatro.config(text = songs[a][buttonorder[3]], activebackground = songs[a][buttonorder[3] + 4])
     else:
         gameover()
         
@@ -52,6 +72,11 @@ def gameover():
     gameovertext.grid(column = 0, row = 4)
     gameovertext2 = Label(window, text = 'Points:' + str(points.get()) + '/10', font = ('Times New Roman', 40), bg = black, fg = grey)
     gameovertext2.grid(column = 0, row = 5)
+
+    amogus = ImageTk.PhotoImage(Image.open('amogus.png'))
+    amoguslabel = Label(image = amogus, background = black)
+    amoguslabel.image = amogus
+    amoguslabel.grid(column = 0, row = 6)
 
 
 mixer.init()
@@ -81,28 +106,20 @@ pointcounter = Label(window, width = 1, height = 1, textvariable = points, font 
 pointcounter.place(x = 450, y = 85)
 
 
-songs = [
-['bert.wav', 'bertyyyyyyyyyyyyyyy', 'charles', 'ryan', 'miggle', green, red, red, red, 7],
-['bert.wav', 'fishy', 'shesh', 'ri call ryna', 'migga', red, red, green, red, 7]
-]
-
-#order = [random.randint(0, len(songs)-1) for i in range(len(songs))] 
-
-
 playbutton = Button(window, width = 8, height = 1, text = 'Play', command = lambda: movebar(songs[a][0], songs[a][9]))
 playbutton.place(x = 30, y = 95)
 
 
-uno = Button(window, width = 70, height = 2, font = ('Times New Roman', 10), text = songs[a][1], background = black, foreground = 'white', activebackground = songs[a][5], command = lambda: answer(1))
+uno = Button(window, width = 70, height = 2, font = ('Times New Roman', 10), text = songs[a][buttonorder[0]], background = black, foreground = 'white', activebackground = songs[a][4 + buttonorder[0]], command = lambda: answer(1))
 uno.grid(row = 3, column = 0, padx = 10, pady = 15)
 
-dos = Button(window, width = 70, height = 2, font = ('Times New Roman', 10), text = songs[a][2], background = black, foreground = 'white', activebackground = songs[a][6], command = lambda: answer(2))
+dos = Button(window, width = 70, height = 2, font = ('Times New Roman', 10), text = songs[a][buttonorder[1]], background = black, foreground = 'white', activebackground = songs[a][4 + buttonorder[1]], command = lambda: answer(2))
 dos.grid(row = 4, column = 0, padx = 10, pady = 15)
 
-tres = Button(window, width = 70, height = 2, font = ('Times New Roman', 10), text = songs[a][3], background = black, foreground = 'white', activebackground = songs[a][7], command = lambda: answer(3))
+tres = Button(window, width = 70, height = 2, font = ('Times New Roman', 10), text = songs[a][buttonorder[2]], background = black, foreground = 'white', activebackground = songs[a][4 + buttonorder[2]], command = lambda: answer(3))
 tres.grid(row = 5, column = 0, padx = 10, pady = 15)
 
-quatro = Button(window, width = 70, height = 2, font = ('Times New Roman', 10), text = songs[a][4], background = black, foreground = 'white', activebackground = songs[a][8], command = lambda: answer(4))
+quatro = Button(window, width = 70, height = 2, font = ('Times New Roman', 10), text = songs[a][buttonorder[3]], background = black, foreground = 'white', activebackground = songs[a][4 + buttonorder[3]], command = lambda: answer(4))
 quatro.grid(row = 6, column = 0, padx = 10, pady = 15)
     
 window.mainloop()
---
+++ TEST.py
@@ -0,0 +1,4 @@
+import random
+
+songorder = random.sample(range(10), 10)
+print(songorder)
